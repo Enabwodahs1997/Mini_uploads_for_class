4dee1e450edee7c5d90c84ed73955dda2fab46e2
Enabwodahs1997 <sesolstice1997@gmail.com>
2026-02-03 10:36:03 -0700

    add assignment files

---
+++ assignment.js
@@ -0,0 +1,30 @@
+function alertMessage(message) {
+    alert(message);
+}
+
+alertMessage("Hello, this is my first JavaScript assignment!");
+// I will update this file as we progress through the assignments.
+// Stay tuned for more updates!
+
+function getWelcomeMessage(name) {
+    alert(`Welcome to the class, ${name}!`);
+}
+
+const userName = prompt("Please enter your name:");
+
+getWelcomeMessage(userName);
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(20, 5));
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(5, 4));
+
+// Additional functions and features will be added in future assignments.
+
