177deb9084938707deca3056c389fd8ca2d114d0
Enabwodahs1997 <sesolstice1997@gmail.com>
2026-02-06 13:24:28 -0700

    added the last two assignments to the assignment.js file that i missed while sick

--- assignment.js
+++ assignment.js
@@ -27,4 +27,41 @@ function multiply(a, b) {
 console.log(multiply(5, 4));
 
 // Additional functions and features will be added in future assignments.
+//math functions for the 2nd assignment
+function getCircleArea(radius) {
+    return Math.PI * radius * radius;
+}
 
+console.log(getCircleArea(5));
+
+function generateRandomPassword(length) {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+    let password = "";
+
+    for (let i = 0; i < length; i += 1) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    return password;
+}
+
+console.log(generateRandomPassword(12));
+
+function calculateTotalWithTax(price, taxRate) {
+    return price + (price * taxRate);
+}
+
+console.log(calculateTotalWithTax(100, 0.07));
+
+// boolean function for the 3rd assignment
+
+let age = 20;
+
+let isStudent = true;
+
+if (age < 18 || isStudent) {
+    console.log("Discount ticket granted ✅");
+} else {
+    console.log("Regular ticket only ❌");
+}
